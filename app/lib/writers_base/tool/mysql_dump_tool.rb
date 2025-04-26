57d1306632c116958d53559f090d25159c7f1c55
Tatsuya Koishi <tkoishi@b-shock.co.jp>
2025-04-27 06:39:57 +0900

    update: MysqlDumpTool

--- app/lib/writers_base/tool/mysql_dump_tool.rb
+++ app/lib/writers_base/tool/mysql_dump_tool.rb
@@ -1,18 +1,20 @@
 module WritersBase
-  class MysqlDumpTool < Tool
+  class MysqldumpTool < Tool
     def exec(args = {})
       result = {success: [], delete: [], failure: []}
       databases.each do |db|
         dir = File.join(dest_dir, db)
         FileUtils.mkdir_p(dir)
-        path = File.join(dir, "#{db}_#{Time.now.strftime('%Y-%m-%d')}.sql.gz")
-        result[:success].push(dump(path, {host:, user:, password:, port:, db:}))
-        finder(dir).execute do |f|
-          File.unlink(f)
-          result[:delete].push(f)
-        end
+        path = dump_path(db, dir)
+
+        logger.info(tool: underscore, db:, message: 'ダンプ開始')
+        dump(path, host:, user:, password:, port:, db:)
+        logger.info(tool: underscore, db:, path:, message: 'ダンプ完了')
+        result[:success] << path
+        result[:delete].concat(delete_old_files(dir))
       rescue => e
-        result[:failure].push(path:, error: e.message.strip)
+        logger.error(tool: underscore, db:, error: e.message.strip, class: e.class.to_s)
+        result[:failure] << {db:, error: e.message.strip, class: e.class.to_s}
       end
       return result
     end
@@ -30,26 +32,39 @@ module WritersBase
         '-u', params[:user],
         '--port', params[:port],
         params[:db],
-        '--single-transaction',
-        '--skip-dump-date',
         :|, 'gzip',
-        :>, Shellwords.escape(path)
+        :>, path
       ])
       command.env = {'MYSQL_PWD' => params[:password]}
-      return if Environment.test?
+      return if test_mode?
       command.exec
       FileUtils.chmod(0o640, path)
       FileUtils.chown('root', 'adm', path)
-      return path
+    end
+
+    def delete_old_files(dir)
+      deleted = []
+      finder(dir).execute do |f|
+        logger.info(tool: underscore, file: f, message: 'ファイル削除')
+        File.unlink(f)
+        deleted << f
+      end
+      if deleted.empty?
+        logger.warn(tool: underscore, dir:, message: '削除対象ファイルなし')
+      end
+      deleted
     end
 
     def finder(dir)
       finder = Ginseng::FileFinder.new
       finder.dir = dir
-      finder.patterns = ['*.log']
       finder.patterns = ['*.sql.gz']
       finder.mtime = days
       return finder
+    end
+
+    def dump_path(db, dir)
+      return File.join(dir, "#{db}_#{Time.now.strftime('%Y-%m-%d')}.sql.gz")
     end
 
     def dest_dir
